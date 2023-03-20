aa0f82054d98d5e1ddd9c76849cf372312f89f6b
Simon Langlois <simonlanglois.sl@gmail.com>
2023-03-20 16:22:16 -0400

    Implement request handlers and database/file access.

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,17 +1,16 @@
 package server;
 
 import javafx.util.Pair;
+import server.models.Course;
+import server.models.RegistrationForm;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class Server {
-
+    public final static String DATABASE_PATH = "./src/main/java/server/data";
     public final static String REGISTER_COMMAND = "INSCRIRE";
     public final static String LOAD_COMMAND = "CHARGER";
     private final ServerSocket server;
@@ -76,10 +75,14 @@ public class Server {
     }
 
     public void handleEvents(String cmd, String arg) {
-        if (cmd.equals(REGISTER_COMMAND)) {
-            handleRegistration();
-        } else if (cmd.equals(LOAD_COMMAND)) {
-            handleLoadCourses(arg);
+        try {
+            if (cmd.equals(REGISTER_COMMAND)) {
+                handleRegistration();
+            } else if (cmd.equals(LOAD_COMMAND)) {
+                handleLoadCourses(arg);
+            }
+        } catch (Exception ex) {
+            // Ignore File errors ???
         }
     }
 
@@ -87,11 +90,39 @@ public class Server {
      Lire un fichier texte contenant des informations sur les cours et les transofmer en liste d'objets 'Course'.
      La méthode filtre les cours par la session spécifiée en argument.
      Ensuite, elle renvoie la liste des cours pour une session au client en utilisant l'objet 'objectOutputStream'.
-     @param arg la session pour laquelle on veut récupérer la liste des cours
+     @param session la session pour laquelle on veut récupérer la liste des cours
      @throws Exception si une erreur se produit lors de la lecture du fichier ou de l'écriture de l'objet dans le flux
      */
-    public void handleLoadCourses(String arg) {
-        // TODO: implémenter cette méthode
+    public void handleLoadCourses(String session) throws Exception {
+        File database = new File(DATABASE_PATH + "/cours.txt");
+        try (Scanner scanner = new Scanner(new FileInputStream(database))) {
+            List<Course> courses = parseCourseList(scanner, session);
+            objectOutputStream.writeObject(courses);
+            objectOutputStream.flush();
+        }
+    }
+
+    private List<Course> parseCourseList(Scanner scanner, String sessionFilter) {
+        List<Course> courses = new ArrayList<Course>();
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            Course course = parseCourse(line);
+            if (course.getSession().equals(sessionFilter)) {
+                courses.add(course);
+            }
+        }
+        return courses;
+    }
+
+    private Course parseCourse(String line) {
+        Scanner scanner = new Scanner(line);
+        scanner.useDelimiter("\t");
+
+        String code = scanner.next();
+        String name = scanner.next();
+        String session = scanner.next();
+
+        return new Course(name, code, session);
     }
 
     /**
@@ -99,8 +130,32 @@ public class Server {
      et renvoyer un message de confirmation au client.
      @throws Exception si une erreur se produit lors de la lecture de l'objet, l'écriture dans un fichier ou dans le flux de sortie.
      */
-    public void handleRegistration() {
-        // TODO: implémenter cette méthode
+    public void handleRegistration() throws Exception {
+        RegistrationForm form = (RegistrationForm) objectInputStream.readObject();
+
+        boolean append = true;
+        File database = new File(DATABASE_PATH + "/inscription.txt");
+        try (PrintWriter out = new PrintWriter(new FileWriter(database, append))) {
+            String line = serializeRegistration(form);
+            out.println(line);
+            out.flush();
+
+            objectOutputStream.writeObject("OK");
+            objectOutputStream.flush();
+        }
+    }
+
+    private static String serializeRegistration(RegistrationForm form) {
+        String[] fields = new String[] {
+                form.getCourse().getSession(),
+                form.getCourse().getCode(),
+                form.getMatricule(),
+                form.getPrenom(),
+                form.getNom(),
+                form.getEmail()
+        };
+
+        return String.join("\t", fields);
     }
 }
 
