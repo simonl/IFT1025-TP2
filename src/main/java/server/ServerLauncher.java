30560337847bb8e38539330235b96acf18f3cf72
Simon Langlois <simonlanglois.sl@gmail.com>
2023-04-15 14:26:22 -0400

    Add comments for RegistrationForm.

--- src/main/java/server/ServerLauncher.java
+++ src/main/java/server/ServerLauncher.java
@@ -4,12 +4,13 @@ package server;
  * La classe ServerLauncher permet d'exécuter le server.
  */
 public class ServerLauncher {
+    // Port de connexion au serveur
     public final static int PORT = 1337;
 
     /**
-     * La méthode main permet de lancer le serveur.
-     * Elle vérifie également qu'il n'y a pas d'erreur lors de l'exécution.
-     * @param args
+     * La méthode main permet de lancer le serveur sur le port 1337.
+     *
+     * @param args Aucun arguments ne sont attendus sur la ligne de commande
      */
     public static void main(String[] args) {
         Server server;
