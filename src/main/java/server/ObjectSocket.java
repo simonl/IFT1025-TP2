c9fffc6e9e54912c95e22d354444ccc9fce1b2eb
Simon Langlois <simonlanglois.sl@gmail.com>
2023-04-17 01:34:55 -0400

    Add comments to ObjectSocket.

--- src/main/java/server/ObjectSocket.java
+++ src/main/java/server/ObjectSocket.java
@@ -5,33 +5,62 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
+/**
+ * La classe ObjectSocket encapsule la communication TCP à base d'objets sérialisés.
+ */
 public class ObjectSocket implements Channel, AutoCloseable {
     private final Socket client;
     private final ObjectInputStream objectInputStream;
     private final ObjectOutputStream objectOutputStream;
 
-    public ObjectSocket(Socket client) throws IOException {
-        this.client = client;
+    /**
+     * Le constructeur écrit immédiatement un entête sur la connexion TCP donnée, pour établir le protocol ObjectStream.
+     * On s'attend aussi à ce que le pair ait fait pareil, et on lit l'entête provenant du socket.
+     *
+     * @param socket La connexion TCP déjà établie avec un pair
+     * @throws IOException Si l'établissement du protocol échoue
+     */
+    public ObjectSocket(Socket socket) throws IOException {
+        this.client = socket;
 
-        // Le ObjectOutputStream doit être instancié en premier de chaque côté (client/serveur),
-        // car il écrit un header qui est attendu par le ObjectInputStream de l'autre côté (serveur/client).
-        this.objectOutputStream = new ObjectOutputStream(client.getOutputStream());
+        // Le ObjectOutputStream doit être instancié en premier de chaque côté (socket/serveur),
+        // car il écrit un header qui est attendu par le ObjectInputStream de l'autre côté (serveur/socket).
+        this.objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
         this.objectOutputStream.flush();
 
-        this.objectInputStream = new ObjectInputStream(client.getInputStream());
+        this.objectInputStream = new ObjectInputStream(socket.getInputStream());
     }
 
+    /**
+     * Lire un object d'un type donné provenant de la connexion.
+     *
+     * @return Un object désérialisé de type T
+     * @param <T> Le type d'objet qu'on s'attend à recevoir
+     * @throws Exception Si la lecture échoue ou on reçoit un objet inattendu
+     */
     public <T> T read() throws Exception {
         return (T) this.objectInputStream.readObject();
     }
 
-    public <T> void write(T o) throws Exception {
+    /**
+     * Écrire un objet en format sérialisé sur la connexion.
+     *
+     * @param o L'objet qu'on veut transmettre
+     * @param <T> Le type de l'objet
+     * @throws Exception Si l'écriture échoue
+     */
+    public <T> void write(T o) throws IOException {
         objectOutputStream.writeObject(o);
         objectOutputStream.flush();
     }
 
+    /**
+     * Fermer la connexion TCP proprement.
+     *
+     * @throws IOException Si une erreur I/O
+     */
     @Override
-    public void close() throws Exception {
+    public void close() throws IOException {
         this.objectOutputStream.close();
         this.objectInputStream.close();
         this.client.close();
